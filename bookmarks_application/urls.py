ba713372ee8d89a7d2bc9ba75e27030e184ca6ac
Daniel Moraes <dmrs07@gmail.com>
2016-12-26 22:10:19 -0200

    Revert "Removendo db..."
    This reverts commit 7711495170ef7b26bd4564f5c36b610fa319406d.

--- bookmarks_application/urls.py
+++ bookmarks_application/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     url(r'^user/(?P<username>[-\w]+)/$',  views.bookmark_user, name='bookmark_user'),
     url(r'^create/$', views.bookmark_create, name='bookmark_create'),
     url(r'^edit/(?P<pk>\d+)/$', views.bookmark_edit, name='bookmark_edit'),
-    url(r'^delete/(?P<pk>\d+)/$', views.bookmark_delete, name='bookmark_delete'),
     url(r'^$', views.bookmark_list, name='bookmark_list'),
 
 ]
